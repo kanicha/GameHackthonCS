d93cddaf028ec74c052e75e998df0743ed2849e4
torakenosu8901 <torakenosu8901@gmaul.com>
2022-05-14 18:29:41 +0900

    フェードインフェードアウトの処理の途中 前途多難である

---
+++ Assets/Resources/Users/toranosuke/Scripts/FadeContoller.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeContoller : MonoBehaviour
+{
+
+
+    private static Canvas canvas;
+
+    private static Image image;
+
+    private bool _canLoadScene = true;
+
+    private static void Init()
+    {
+        GameObject canvasObject = new GameObject("CanvasFade");
+        canvas = canvasObject.AddComponent<Canvas>();
+        canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+        canvas.sortingOrder = 100;
+        canvasObject.AddComponent<GraphicRaycaster>();
+
+        image = new GameObject("ImageFade").AddComponent<Image>();
+        image.transform.SetParent(canvas.transform, false);
+        image.rectTransform.anchoredPosition = Vector3.zero;
+        Camera camera = Camera.main;
+        image.rectTransform.sizeDelta = new Vector2(Screen.width, Screen.height);
+        image.color = Color.clear;
+        image.raycastTarget = false;
+
+        DontDestroyOnLoad(canvas.gameObject);
+       // DontDestroyOnLoad(instance.gameObject);
+       // canvasObject.AddComponent<Transition>();
+    }
+    private void Start()
+    {
+        Init();
+    }
+
+   /* public void LoadScene(float interval, GameScene gamescene)
+    {
+        if (_canLoadScene)
+        {
+            StartCoroutine(Fade(interval, gamescene.ToString()));
+        }
+    }*/
+
+    public IEnumerator Fade(float interval, string taransition2)
+    {
+        yield return FadeOut(interval);
+
+       // yield return SceneManager.LoadSceneAsync(taransition2);
+
+        yield return FadeIn(interval);
+    }
+
+    public IEnumerator FadeOut(float interval)
+    {
+        _canLoadScene = false;
+        float time = 0f;
+      //  Canvas.enabled = true;
+
+        while (time <= interval)
+        {
+            float fadeAlpha = Mathf.Lerp(0f, 1f, time / interval);
+            //Image.color = new Color(0.0f, 0.0f, 0.0f, fadeAlpha);
+            time += Time.deltaTime;
+            yield return null;
+        }
+    }
+
+    public IEnumerator FadeIn(float interval)
+    {
+        _canLoadScene = true;
+        float time = 0f;
+        time = 0f;
+        while (time <= interval)
+        {
+            float fadeAlpha = Mathf.Lerp(1f, 0f, time / interval);
+            //Image.color = new Color(0.0f, 0.0f, 0.0f, fadeAlpha);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        //Canvas.enabled = false;
+    }
+
+
+}
--- Assets/Resources/Users/toranosuke/Scripts/SceneTrasitionFade.cs
+++ Assets/Resources/Users/toranosuke/Scripts/SceneTrasitionFade.cs
@@ -7,42 +7,116 @@ using UnityEngine.SceneManagement;
 
 public class SceneTrasitionFade : MonoBehaviour
 {
+    //インスタンスの作成
+    public static SceneTrasitionFade instanc;
+
+    //遷移先のシーン名
+    [SerializeField]
+    private string nextScene;
+
     //遷移を隠すためのイメージ
     [SerializeField]
     private GameObject fadecurtain;
 
     //フェードの速度
-    private float fadespeed = 0.5f;
+    private float fadespeed = 0.02f;
 
-    //フェードの開始を合図するbool型変数
-    private bool fadestart;
+    //フェードアウトを始めるためのbool型変数
+    [SerializeField]
+    private bool fadeout = false;
 
-    // Start is called before the first frame update
-    void Start()
+    //フェードインを始めるためのbool型変数
+    [SerializeField]
+    private bool fadein = true;
+
+    //フェードインを行う関数
+    private void FadeIn()
     {
-        //フェード用イメージの透明度を初期化(完全透明がデフォルト)
         Image image = fadecurtain.GetComponent<Image>();
-        image.color = new Color(0f, 0f, 0f, 0.0f);   
-        //フェード用カーテンを非表示にする
-        fadecurtain.SetActive(false);
+        image.color = new Color(0,0,0,1f);
+
+        while(fadein)
+        {
+            image.color -= new Color(0f, 0f, 0f, fadespeed * Time.deltaTime);
+            if(image.color.a <= 0.1f)
+            {
+                image.color = new Color(0,0,0,0f);
+                fadein = false;
+            }
+        }            
     }
 
-    // Update is called once per frame
-    void Update()
+    //フェードアウトを行う関数
+    private void FadeOut()
+    {
+        Image image = fadecurtain.GetComponent<Image>();
+        image.color = new Color(0,0,0,0f);
+
+        while(fadeout)
+        {
+            image.color += new Color(0f, 0f, 0f, fadespeed * Time.deltaTime);
+            if(image.color.a >= 0.9f)
+            {
+                image.color = new Color(0,0,0,1f);
+                fadeout = false;    
+                //シーンを遷移させるコード
+                SceneManager.LoadScene($"{nextScene}");            
+            }
+        }            
+    }
+
+    //他のスクリプトからフェードアウトとシーン遷移を行うための関数
+    public void FadeStart()
+    {
+        fadeout = true;
+    }
+
+    void Start() {
+        //シーンを切り替えた直後にフェードインを実行
+        FadeIn();
+    }
+
+    void FiUpdate()
     {
         if(Input.GetKeyDown(KeyCode.Return))
         {
-            fadestart = true;
+            //FadeStart();
         }
 
-        if(fadestart)
+        if(fadein)
         {
-            fadecurtain.SetActive(true);
-
             Image image = fadecurtain.GetComponent<Image>();
-            image.color += new Color(0f, 0f, 0f, fadespeed * Time.deltaTime);
+            image.color = new Color(0,0,0,1f);
+
+            while(fadein)
+            {
+                image.color -= new Color(0f, 0f, 0f, fadespeed * Time.deltaTime);
+                if(image.color.a <= 0.1f)
+                {
+                    image.color = new Color(0,0,0,0f);
+                    fadein = false;
+                }
+            }
         }
 
+        if(fadeout)
+        {
+            //フェードアウトを実行
+            //FadeOut();
+            Image image = fadecurtain.GetComponent<Image>();
+            image.color = new Color(0,0,0,0f);
 
+            while(fadeout)
+            {
+                image.color += new Color(0f, 0f, 0f, fadespeed * Time.deltaTime);
+                if(image.color.a >= 0.9f)
+                {
+                    image.color = new Color(0,0,0,1f);
+                    fadeout = false;    
+                    //シーンを遷移させるコード
+                    SceneManager.LoadScene($"{nextScene}");            
+                }
+            }
+        }            
     }
 }
