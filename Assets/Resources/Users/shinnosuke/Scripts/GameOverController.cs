65e042b1bfd0a574347e277c0dd6c57e88639edd
aoyadoshinnosuke <shinaaa01204@gmail.com>
2022-05-17 00:45:21 +0900

    ゲームオーバー画面のカーソルの移動プログラム移動実装

--- Assets/Resources/Users/shinnosuke/Scripts/GameOverController.cs
+++ Assets/Resources/Users/shinnosuke/Scripts/GameOverController.cs
@@ -7,21 +7,54 @@ public class GameOverController : MonoBehaviour
 {
     [SerializeField] Image _curor;
     [SerializeField] GameObject[] _titleText;
-   
+    [SerializeField] float _cursorLength = -500;
+    private int _selectNum  =0;
+    Vector2 vec = Vector2.zero;
+
     // Start is called before the first frame update
     void Start()
     {
-        Vector2 vec = Vector2.zero;
         vec = _titleText[0].transform.position;
+        vec += new Vector2(_cursorLength, 0);
         _curor.transform.position = vec;
 
         // XŽ²‚ð‚©‚³‚ñ
     }
 
-
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+
+            _selectNum--;
+            SelectText();
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            _selectNum++;
+            SelectText();
+        }
+
+
+    }
+
+    private void SelectText()
+    {
+        if (_selectNum >= _titleText.Length)
+        {
+            _selectNum = _titleText.Length - 1;
+        }
+        else if (_selectNum <= 0) 
+        {
+            _selectNum = 0;
+
+        }
+
+
+            vec = _titleText[_selectNum].transform.position;
+        vec += new Vector2(_cursorLength, 0);
+        _curor.transform.position = vec;
+
     }
 }
