6dce6980987017b5f4f9b2a82c4d75c7baff6b2a
torakenosu <torakenosu8901@gmail.com>
2022-05-16 14:31:24 +0900

    プログラムマッチョの圧倒的筋肉により作成された神コードをト〇スした「シーン遷移」の完成系

--- Assets/Resources/Users/toranosuke/Scripts/TestScript.cs
+++ Assets/Resources/Users/toranosuke/Scripts/TestScript.cs
@@ -4,19 +4,12 @@ using UnityEngine;
 
 public class TestScript : MonoBehaviour
 {
-    GameScene Title;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.Return))
         {
-            //FadeContoller.LoadScene(3f, Title);
+            FadeContoller.Instance.LoadScene(3f, GameScene.Title);
         }
     }
 }
