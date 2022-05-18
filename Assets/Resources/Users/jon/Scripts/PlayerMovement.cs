1a03d42b26729d6547bd5c4827a6ce3855b9ad89
jon <joonaan7@gmail.com>
2022-05-18 16:53:44 +0900

    カメラスクリプト修正

--- Assets/Resources/Users/jon/Scripts/PlayerMovement.cs
+++ Assets/Resources/Users/jon/Scripts/PlayerMovement.cs
@@ -11,7 +11,6 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D rb;
     private bool facingRight = true;
     private float moveDirection;
-    private bool isJumping = false;
 
     // Start is called before the first frame update
     void Start()
