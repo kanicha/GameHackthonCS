7212f7298e763ce5183ed6fb12f536c2423b6b31
SakamakiDaiki <k4n1jp@yahoo.co.jp>
2022-05-19 16:27:33 +0900

    delete: いらないコメントの削除

--- Assets/Resources/Scripts/AttackColliderCheck.cs
+++ Assets/Resources/Scripts/AttackColliderCheck.cs
@@ -5,18 +5,11 @@ using UnityEngine;
 
 public class AttackColliderCheck : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D col)
-    {
-      
-    }
-
     private void OnTriggerEnter2D(Collider2D col)
     {
         // 攻撃を行ったときに相手のモードと自分のモードが違うときに攻撃を通す 
         if (col.transform.tag.Equals("Enemy") && PlayerStatus.playerModeState != col.gameObject.GetComponent<EnemyControler>().enemyMode)
         {
-            Debug.Log("enemy");
-
             // EnemyオブジェクトのHP実数値を習得してきてPlayerのATK分値を減らす
             col.gameObject.GetComponent<EnemyControler>()._localHp -= PlayerStatus.atk;
         }
